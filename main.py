ee14f61d0eeb4655586e6068fdbd1ee938043bdb
ヒロ_フジ <257928059+hirofujikoto@users.noreply.github.com>
2026-02-02 15:10:44 +0900

    create2

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-go go
-123
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "領収書保管アプリ"
+    # 画面に文字を表示
+    text = ft.Text(value="こんにちは！iPhoneから更新しました。")
+    page.add(text)
+
+# アプリを実行
+ft.app(target=main)
